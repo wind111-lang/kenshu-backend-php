e2dc33e418e89f6ab59b712308330f3447fd60ac
wind111-lang <shota.tsutsui@prtimes.co.jp>
2024-05-22 08:10:21 +0900

    RDB設計で行ったSQLを反映

--- docker/postgres/init.sql
+++ docker/postgres/init.sql
@@ -4,7 +4,16 @@ CREATE TABLE "userinfo" (
                             "email" varchar(255),
                             "password" varchar(255),
                             "created_at" datetime,
-                            "updated_at" datetime
+                            "updated_at" datetime,
+);
+
+CREATE TABLE "log_userinfo" (
+                                "id" integer PRIMARY KEY,
+                                "username" varchar(255) UNIQUE,
+                                "email" varchar(255),
+                                "password" varchar(255),
+                                "created_at" datetime,
+                                "deleted_at" datetime,
 );
 
 CREATE TABLE "posts" (
@@ -12,15 +21,27 @@ CREATE TABLE "posts" (
                          "user_id" integer,
                          "posted_at" datetime,
                          "updated_at" datetime,
-                         "thumb_img_url" text,
                          "title" varchar(255),
-                         "body" text
+                         "body" text,
 );
 
-CREATE TABLE "post_image" (
-                              "id" integer PRIMARY KEY,
-                              "post_id" integer,
-                              "img_url" text
+CREATE TABLE "log_posts" (
+                             "id" integer PRIMARY KEY,
+                             "user_id" integer,
+                             "posted_at" datetime,
+                             "deleted_at" datetim
+                                 "title" varchar(255),
+                             "body" text,
+);
+
+CREATE TABLE "thumb_image" (
+                               "post_id" integer PRIMARY KEY,
+                               "image_id" integer
+);
+
+CREATE TABLE "post_images" (
+                               "id" integer PRIMARY KEY
+                                   "img_url" text
 );
 
 CREATE TABLE "post_tags" (
@@ -34,13 +55,21 @@ CREATE TABLE "tags" (
 );
 
 SELECT * FROM posts RIGHT OUTER JOIN post_tags ON posts.id = post_tags.post_id;
+SELECT * FROM posts RIGHT OUTER JOIN thumb_image ON posts.id = thumb_image.post_id;
+SELECT * FROM log_posts RIGHT OUTER JOIN post_tags ON log_posts.id = post_tags.post_id;
 
 ALTER TABLE "userinfo" ADD FOREIGN KEY ("id") REFERENCES "posts" ("user_id");
 
+ALTER TABLE "log_userinfo" ADD FOREIGN KEY ("id") REFERENCES "log_posts" ("user_id");
+
 ALTER TABLE "posts" ADD FOREIGN KEY ("id") REFERENCES "post_tags" ("post_id");
 
-ALTER TABLE "posts" ADD FOREIGN KEY ("id") REFERENCES "post_image" ("post_id");
+ALTER TABLE "posts" ADD FOREIGN KEY ("id") REFERENCES "thumb_image" ("post_id");
 
-ALTER TABLE "post_image" ADD FOREIGN KEY ("img_url") REFERENCES "posts" ("thumb_img_url");
+ALTER TABLE "post_images" ADD FOREIGN KEY ("id") REFERENCES "thumb_image" ("image_id");
+
+ALTER TABLE "log_posts" ADD FOREIGN KEY ("id") REFERENCES "post_tags" ("post_id");
+
+ALTER TABLE "log_posts" ADD FOREIGN KEY ("id") REFERENCES "post_image" ("post_id");
 
 ALTER TABLE "tags" ADD FOREIGN KEY ("id") REFERENCES "post_tags" ("tag_id");
