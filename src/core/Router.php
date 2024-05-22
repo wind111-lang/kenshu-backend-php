c32dff1ac07e573225bfb271a4ff39a7f2aaed09
wind111-lang <shota.tsutsui@prtimes.co.jp>
2024-05-22 16:49:00 +0900

    URL取得部分を変更

--- src/core/Router.php
+++ src/core/Router.php
@@ -1,10 +1,52 @@
 <?php
+
 namespace App\core;
 class Router
 {
-    public function route()
+    public function __construct() {
+        echo "This is Router class\n";
+    }
+    public static function route()
     {
-        //TODO: Routingをする
-        echo 'Routing';
+        $url = $_SERVER['REQUEST_URI'] ?? '/';
+        $url = rtrim($url, '/');
+        $url = filter_var($url, FILTER_SANITIZE_URL);
+        $url = explode('/', $url);
+
+
+        $controller = !empty($url[0]) ? 'App\\app\\controllers\\' . ucfirst($url[0]) .
+            'Controller' : 'App\\app\\controllers\\WebController';
+        $method = !empty($url[1]) ? $url[1] : 'index';
+
+        if (class_exists($controller)){
+            $controller = new $controller;
+
+            if (method_exists($controller, $method)){
+                $httpMethod = $_SERVER['REQUEST_METHOD'];
+                switch ($httpMethod){
+                    case 'POST':
+                        $controller->{$method}($_POST);
+                        break;
+                    case 'GET':
+                        $controller->{$method}($_GET);
+                        break;
+                    case 'DELETE':
+                        parse_str(file_get_contents('php://input'), $_DELETE);
+                        $controller->{$method}($_DELETE);
+                        break;
+                    case 'PATCH':
+                        parse_str(file_get_contents('php://input'), $_PATCH);
+                        $controller->{$method}($_PATCH);
+                        break;
+                    default:
+                        echo 'HTTP method not allowed';
+                        break;
+                }
+            }else{
+                echo 'Method not found';
+            }
+        }else{
+            echo 'Controller not found';
+        }
     }
 }
--- src/public/index.php
+++ src/public/index.php
@@ -4,6 +4,6 @@ require_once '../vendor/autoload.php';
 
 use App\core\Router;
 
-$router = new Router();
-$router->route();
+new Router();
+
 
