fa97243161e63a334a517f4b028528aa11a91c7e
wind111-lang <shota.tsutsui@prtimes.co.jp>
2024-05-24 09:13:58 +0900

    Docker configの変更, 投稿表示用のSQLに一時的に変更

--- docker/postgres/master-init.sql
+++ docker/postgres/master-init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE "userinfo" (
-                            "id" SERIAL PRIMARY KEY,
+                            "id" integer PRIMARY KEY,
                             "username" varchar(255) UNIQUE,
                             "email" varchar(255),
                             "password" varchar(255),
@@ -33,3 +33,31 @@ CREATE TABLE "log_posts" (
                              "title" varchar(255),
                              "body" text
 );
+
+CREATE TABLE "thumb_image" (
+                               "post_id" integer PRIMARY KEY,
+                               "image_id" integer UNIQUE
+);
+
+CREATE TABLE "post_images" (
+                               "id" integer PRIMARY KEY,
+                               "img_url" text
+);
+
+CREATE TABLE "post_tags" (
+                             "post_id" integer PRIMARY KEY,
+                             "tag_id" integer UNIQUE
+);
+
+CREATE TABLE "tags" (
+                        "id" integer PRIMARY KEY,
+                        "tag_name" char(32)
+);
+
+ALTER TABLE "userinfo" ADD FOREIGN KEY ("id") REFERENCES "posts" ("user_id");
+ALTER TABLE "posts" ADD FOREIGN KEY ("id") REFERENCES "post_tags" ("post_id");
+ALTER TABLE "tags" ADD FOREIGN KEY ("id") REFERENCES "post_tags" ("tag_id");
+ALTER TABLE "posts" ADD FOREIGN KEY ("id") REFERENCES "thumb_image" ("post_id");
+ALTER TABLE "log_posts" ADD FOREIGN KEY ("id") REFERENCES "post_tags" ("post_id");
+ALTER TABLE "log_posts" ADD FOREIGN KEY ("user_id") REFERENCES "log_userinfo" ("id");
+ALTER TABLE "post_images" ADD FOREIGN KEY ("id") REFERENCES "thumb_image" ("image_id");
---
+++ src/app/helper/CsrfHelper.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\app\helper;
+
+class CsrfHelper
+{
+
+}
